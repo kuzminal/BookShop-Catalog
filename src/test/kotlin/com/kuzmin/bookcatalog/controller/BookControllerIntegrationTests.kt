49cc22ef15ddb4c1bdbecd3bc37d82f95b511f93
kuzminal <kuzmin.al35@gmail.com>
2021-10-14 14:25:12 +0300

    Заменил в тесте restTemplate на webClient

--- src/test/kotlin/com/kuzmin/bookcatalog/controller/BookControllerIntegrationTests.kt
+++ src/test/kotlin/com/kuzmin/bookcatalog/controller/BookControllerIntegrationTests.kt
@@ -1,29 +1,31 @@
 package com.kuzmin.bookcatalog.controller
 
+import com.kuzmin.bookcatalog.api.BookController
 import com.kuzmin.bookcatalog.model.entity.Book
 import com.kuzmin.bookcatalog.service.BookService
-import org.assertj.core.api.Assertions.assertThat
 import org.bson.types.ObjectId
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
+@SpringBootTest
 class BookControllerIntegrationTests {
-    val testRestTemplate: TestRestTemplate = TestRestTemplate()
 
     @Test
-    fun whenPostRequestThenBookCreated() {
-        val expectedBook = Book(ObjectId.get(), "9783161484101", "Title", "Author", "1991", 9.90)
-        val response: ResponseEntity<Book> = testRestTemplate.postForEntity(
-            "http://localhost:9001/books", expectedBook,
-            Book::class.java
-        )
-        assertThat(response.statusCode).isEqualTo(HttpStatus.CREATED)
-        assertThat(response.body).isNotNull
-        assertThat(response.body?.isbn.orEmpty()).isEqualTo(expectedBook.isbn)
-        testRestTemplate.delete("http://localhost:9001/books/${expectedBook.isbn}")
+    fun getBook() {
+        val bookService: BookService = Mockito.mock(BookService::class.java)
+        `when`(bookService.findByIsbn("9783161484101"))
+            .thenReturn(Mono.just(Book(ObjectId.get(), "9783161484101", "Title", "Author", "1991", 9.90)))
+        val testClient = WebTestClient.bindToController(
+            BookController(bookService)
+        ).build()
+        testClient.get().uri("/books/9783161484101")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(Book::class.java)
     }
 }
