a2b346eb31736a98acebc1dad9ea168df6b2b791
kuzminal <kuzmin.al35@gmail.com>
2021-10-14 14:56:35 +0300

    Добвил в тест JSON с ответом

--- src/test/kotlin/com/kuzmin/bookcatalog/controller/BookControllerIntegrationTests.kt
+++ src/test/kotlin/com/kuzmin/bookcatalog/controller/BookControllerIntegrationTests.kt
@@ -8,8 +8,11 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.`when`
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.io.ClassPathResource
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.util.StreamUtils
 import reactor.core.publisher.Mono
+import java.nio.charset.Charset
 
 
 @SpringBootTest
@@ -18,14 +21,19 @@ class BookControllerIntegrationTests {
     @Test
     fun getBook() {
         val bookService: BookService = Mockito.mock(BookService::class.java)
+        val book = Book(ObjectId("6104032d71e8ba05acfdebbf"), "9783161484101", "Title", "Author", "1991", 9.90);
         `when`(bookService.findByIsbn("9783161484101"))
-            .thenReturn(Mono.just(Book(ObjectId.get(), "9783161484101", "Title", "Author", "1991", 9.90)))
+            .thenReturn(Mono.just(book))
         val testClient = WebTestClient.bindToController(
             BookController(bookService)
         ).build()
+        val byIsbnResource = ClassPathResource("/books/book-by-isbn.json")
+        val byIsbnJson: String = StreamUtils.copyToString(
+            byIsbnResource.inputStream, Charset.defaultCharset()
+        )
         testClient.get().uri("/books/9783161484101")
             .exchange()
             .expectStatus().isOk
-            .expectBody(Book::class.java)
+            .expectBody().json(byIsbnJson)
     }
 }
